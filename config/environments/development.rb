e2210a6cc917dd9f6c4f630badbf4c0c32a1ae6a
140ch204 <140ch204_github@gadz.org>
2019-11-04 23:52:28 +0100

    V1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Test mail 
-  # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.perform_deliveries = true
+  #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
 
 end
