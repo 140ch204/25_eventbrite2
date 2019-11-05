49058e105cc4c5c722cbe04063a53d120cfe01d0
140ch204 <140ch204_github@gadz.org>
2019-11-05 13:58:19 +0100

    V2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/index'
   get 'static_pages/secret'
   devise_for :users
-  root 'static_pages#index'
+  root 'event#index'
   resources :user
   resources :event
   resources :participation
